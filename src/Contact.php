12f8e58542a585a6d28495a49e74674bfd7f6297
Jordan <jbirdmeier@gmail.com>
2016-02-12 08:30:49 -0800

    add a constructor to the Contact class

--- src/Contact.php
+++ src/Contact.php
@@ -7,6 +7,15 @@ class Contact
     private $address;
     private $email;
 
+    function __construct($contact_name, $contact_photo, $contact_phone, $contact_address, $contact_email)
+    {
+        $this->name = $contact_name;
+        $this->photo = $contact_photo;
+        $this->phone = $contact_phone;
+        $this->address = $contact_address;
+        $this->email = $contact_email;
+    }
+
 }
 
 

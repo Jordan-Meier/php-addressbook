5f5025319bc892d90b1729fdc1257fff334c963c
Jordan <jbirdmeier@gmail.com>
2016-02-12 08:37:49 -0800

    add getters and setters for each private property in Contact class

--- src/Contact.php
+++ src/Contact.php
@@ -16,6 +16,56 @@ class Contact
         $this->email = $contact_email;
     }
 
+    function setName($new_name)
+    {
+        $this->name = (string) $new_name;
+    }
+
+    function getName()
+    {
+        return $this->name;
+    }
+
+    function setPhoto($new_photo)
+    {
+        $this->photo = (string) $new_photo;
+    }
+
+    function getPhoto()
+    {
+        return $this->photo;
+    }
+
+    function setPhone($new_phone)
+    {
+        $this->phone = (string) $new_phone;
+    }
+
+    function getPhone()
+    {
+        return $this->phone;
+    }
+
+    function setAddress($new_address)
+    {
+        $this->address = (string) $new_address;
+    }
+
+    function getAddress()
+    {
+        return $this->address;
+    }
+
+    function setEmail($new_email)
+    {
+        $this->email = (string) $new_email;
+    }
+
+    function getEmail()
+    {
+        return $this->email;
+    }
+
 }
 
 
